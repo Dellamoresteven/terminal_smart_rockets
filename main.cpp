f1719f5b0e04991086115a87d420f7ad911ebb99
Steven.Dellamore <Steven.DA.Dellamore@partner.bmw.de>
2020-08-04 14:57:11 -0500

    kindaworking

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
-// g++ --std=c++17 -o run main.cpp rocket.cpp && ./run
+// g++ --std=c++17 -pthread -o run main.cpp rocket.cpp && time ./run
+/*
+real	0m3.593s
+user	0m3.580s
+sys	0m0.016s
+*/
 #include <stdlib.h>
 #include <iostream>
 #include <cstdlib>
@@ -6,17 +11,19 @@
 #include <stdlib.h>
 #include <math.h>
 #include <algorithm>
+#include <thread>
 
 #include "rocket.cpp"
 
 using namespace std;
 // PARAMS TO CHANGE
-constexpr int moves = 400;
-constexpr int numRockets = 100;
+#define NUM_THREADS 5
+constexpr int moves = 300;
+constexpr int numRockets = 200;
 const std::pair<int,int> targetLoc = std::pair(4,4);
 constexpr double percentTake = .2;
-constexpr double mutationRate = .05;
-constexpr double numGames = 1000;
+constexpr double mutationRate = .03;
+constexpr double numGames = 102;
 // END
 
 #define s 50
@@ -29,7 +36,20 @@ vector<rocket*> newGeneration(vector<rocket*> r);
 int map[s][s];
 int minScore = 1000;
 
+
+void *wait(void *t) {
+
+}
+
 int main() {
+    pthread_t threads[NUM_THREADS];
+    pthread_attr_t attr;
+    void *status;
+
+    // Initialize and set thread joinable
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
     bool gameOver = false;
     initMap();
     srand (time(NULL));
@@ -55,8 +75,14 @@ int main() {
     }
 
     auto playgame = [&](bool show) {
+        // pthread_create(&threads[i], &attr, wait, (void *)i );
         while(!gameOver) {
             if(show) usleep(1000 * 1000 * .01);
+
+            // for(int k = 0; k < NUM_THREADS; k++) {
+            //     pthread_create(&threads[k], &attr, wait, (void *)k );
+            // }
+
             for(int i = 0; i < r.size(); i++) {
                 rocket * r1 = r.at(i);
                 if(!r1->isGodParent) {
@@ -71,6 +97,8 @@ int main() {
                     break;
                 }
             }
+
+
             if(show) drawMap();
         }
     };
@@ -83,6 +111,7 @@ int main() {
             }
         }
         initMap();
+        // std::thread first(playgame(show));
         playgame(show);
         scoreRockets(r);
         r = newGeneration(r);
@@ -91,12 +120,14 @@ int main() {
     for(int i = 0; i < numGames; i++) {
         gameloop(false);
     }
+
     gameloop(true);
 }
 
 void scoreRockets(vector<rocket*> r) {
     int rocketI = 0;
     double avg = 0;
+    minScore = 100;
     for(auto rocket : r) {
         int dist = sqrt(pow(rocket->x - targetLoc.second, 2) + pow(rocket->y - targetLoc.first, 2));
         rocket->score = dist;
@@ -144,7 +175,7 @@ vector<rocket*> newGeneration(vector<rocket*> r) {
         rocket * p1 = r.at(rand() % numTake);
         rocket * p2 = r.at(rand() % numTake);
         vector<std::string> newDNA;
-        for(int j = 0; j < moves/2; j++) {
+        for(int j = 0; j < moves; j++) {
             double rm = rand() % 100/100.0;
             if(rm < mutationRate) {
                 int r = rand() % 4;
@@ -158,25 +189,9 @@ vector<rocket*> newGeneration(vector<rocket*> r) {
                     newDNA.push_back("N");
                 }
             } else {
-                newDNA.push_back(p1->directions.at(j));
-            }
-        }
-
-        for(int j = newDNA.size(); j < moves; j++) {
-            double rm = rand() % 100/100.0;
-            if(rm < mutationRate) {
-                int r = rand() % 4;
-                if(r == 0) {
-                    newDNA.push_back("U");
-                } else if(r == 1) {
-                    newDNA.push_back("R");
-                } else if(r == 2) {
-                    newDNA.push_back("L");
-                }   else if(r == 3) {
-                    newDNA.push_back("N");
-                }
-            } else {
-                newDNA.push_back(p2->directions.at(j));
+                int whichParent = rand() % 2;
+                if(whichParent == 0) newDNA.push_back(p1->directions.at(j));
+                else if(whichParent == 1) newDNA.push_back(p2->directions.at(j));
             }
         }
         // if(rm2 < mutationRate) { // Brand new Random DNA
@@ -216,11 +231,11 @@ void initMap() {
         }
     }
 
-    for(int i = 0; i < s; i++) {
-        if(i != 45) {
-            map[10][i] = 1;
-        }
-    }
+    // for(int i = 0; i < s; i++) {
+    //     if(i != 45) {
+    //         map[10][i] = 1;
+    //     }
+    // }
 
 
     for(int i = 0; i < s; i++) {
